5e5b5f66595bb6c7c2baa5b496d227623b6d7441
juan-preciado <juan.camilo.preciado.2007@gmail.com>
2025-10-27 00:42:52 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
   const formularios = document.querySelectorAll("form");
 
@@ -22,3 +23,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+=======
+document.addEventListener("DOMContentLoaded", () => {
+  const formularios = document.querySelectorAll("form");
+
+  formularios.forEach(form => {
+    form.addEventListener("submit", e => {
+      const inputs = form.querySelectorAll("input[required], textarea[required]");
+      let valid = true;
+
+      inputs.forEach(input => {
+        if (!input.value.trim()) {
+          valid = false;
+          input.style.border = "2px solid red";
+        } else {
+          input.style.border = "2px solid green";
+        }
+      });
+
+      if (!valid) {
+        e.preventDefault();
+        alert("Por favor, completa todos los campos obligatorios.");
+      }
+    });
+  });
+});
+>>>>>>> 7e1dd6c (Cambios de la página)
